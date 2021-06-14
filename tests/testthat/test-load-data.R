26db492a79994d8a8b8ebfff65101f4a5089427d
gprashal0706 <garima_p@ee.iitr.ac.in>
2021-06-14 22:47:50 +0530

    loaddatchanges

--- tests/testthat/test-load-data.R
+++ tests/testthat/test-load-data.R
@@ -1,16 +1,16 @@
 demand.data <- load_demand_data()
 
-test_that("public holidays load", {
+test_that("public holidays load",{
   # Test holidays
-  pub_hol_vec <- ymd(c("2017-8-28", "2017-12-25", "2017-12-26", "2018-1-1", 
-                       "2018-4-2"))
+  pub_hol_vec <- ymd(c("2019-4-18","2019-4-19","2019-4-21",2019-12-25","2019-12-26","2019-1-1", 
+                       "2019-5-17","2019-5-30"))
   pub_hol_vec <- demand.data %>% 
     filter(date(datetime) %in% pub_hol_vec) %>% 
     pull(public_holiday)
   expect_true(all(pub_hol_vec == 1))
   # Test non-holidays
-  pub_hol_vec <- ymd(c("2017-8-27", "2017-12-12", "2017-12-24", "2018-1-2", 
-                       "2018-4-3"))
+  pub_hol_vec <- ymd(c("2019-8-27", "2019-12-12", "2019-12-24", "2019-1-2", 
+                       "201-4-3"))
   pub_hol_vec <- demand.data %>% 
     filter(date(datetime) %in% pub_hol_vec) %>% 
     pull(public_holiday)
@@ -35,21 +35,19 @@ test_that("demand loads correctly", {
   expect_lt(max(demand.summ$demand_mw, na.rm = T), 80)
 })
 
-test_that("leap years adjusted correctly", {
-  expect_equal(max(demand.data$yday), 365)
-  demand.data %>% 
-    filter(between(date(datetime), ymd("2020-01-01"), ymd("2020-02-28"))) %>% 
-    dplyr::distinct(yday) %>% 
-    pull() %>% 
-    expect_equal(1:59)
-  demand.data %>% 
-    filter(date(datetime) == ymd("2020-02-29")) %>% 
-    dplyr::distinct(yday) %>% 
-    pull() %>% 
-    expect_equal(59.5)
-  demand.data %>% 
-    filter(date(datetime) > ymd("2020-02-29")) %>% 
-    dplyr::distinct(yday) %>% 
-    pull() %>% 
-    expect_equal(60:190)
-})
+##test_that("leap years adjusted correctly", {
+ ## expect_equal(max(demand.data$yday), 365)
+  ##demand.data %>% 
+   ## filter(between(date(datetime), ymd("2020-01-01"), ymd("2020-02-28"))) %>% 
+   ## dplyr::distinct(yday) %>% 
+   ###  expect_equal(1:59)
+  ##demand.data %>% 
+  ##  filter(date(datetime) == ymd("2020-02-29")) %>% 
+  ##  dplyr::distinct(yday) %>% 
+   ## pull() %>% 
+   ## expect_equal(59.5)
+  ##demand.data %>% 
+  ##  filter(date(datetime) > ymd("2020-02-29")) %>% 
+   ## dplyr::distinct(yday) %>% 
+   ### expect_equal(60:190)
+##})
