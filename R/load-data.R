e5d284c899d044d466c7ea122760d900ec0e78ce
gprashal0706 <garima_p@ee.iitr.ac.in>
2021-07-03 20:58:11 +0530

    demand-data-updated

--- R/load-data.R
+++ R/load-data.R
@@ -99,7 +99,7 @@ load_pv_data <- function() {
     select(-.data$demand_mw) %>% 
     add_lags(
       lags = list(
-        "pv_power_mw" = 1440*7,
+        "demand_mw" = 1440*7,
         "temp_location3" = 1
        
       )
